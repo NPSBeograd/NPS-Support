0ac26f1c329b92373305305f87a602f5abb23057
Nemanja Zajic <zakizaja@hotmail.com>
2023-10-19 15:56:01 +0200

    a

--- script/CreateSecrets.ps1
+++ script/CreateSecrets.ps1
@@ -17,7 +17,7 @@ New-Item -Path "./LibSodiumNuget" -ItemType Directory
 nuget install libsodium -OutputDirectory "./LibSodiumPackage" -ExcludeVersion
 
 Write-Host "LIBSODIUM"
-Get-ChildItem "./LibSodiumPackage/libsodium"
+Get-ChildItem "./LibSodiumPackage/libsodium/runtimes"
 
 Add-Type -Path "./LibSodiumPackage/runtimes/win-x64/native/libsodium.dll"
 Add-Type -AssemblyName System.Threading.Tasks
