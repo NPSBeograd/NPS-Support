a68305b60386f33b4c512638046f1c5d265854c7
Nemanja Zajic <zakizaja@hotmail.com>
2023-10-19 12:45:28 +0200

    a

--- script/CreateSecrets.ps1
+++ script/CreateSecrets.ps1
@@ -23,7 +23,7 @@ $version= ls /home/runner/.nuget/packages/libsodium
 Expand-Archive -Path "/home/runner/.nuget/packages/libsodium/$version/libsodium.$($version).nupkg" -DestinationPath "LibSodiumPackage"
 
 
-Add-Type -Path "./LibSodiumPackage//runtimes/win-x64/native/libsodium.dll"
+Add-Type -Path "./LibSodiumPackage//runtimes/win-x86/native/libsodium.dll"
 Add-Type -AssemblyName System.Threading.Tasks
 Add-Type -AssemblyName System.Security
 
