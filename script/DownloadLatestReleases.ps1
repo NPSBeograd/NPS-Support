fd8f1225cba3ff31431c5ccb7ac11f6af24d74ae
Nemanja Zajic <34772552+nemanja899@users.noreply.github.com>
2023-10-19 10:42:57 +0200

    deploy all extensions script (#33)
    * automatic reporting
    
    * Change read me
    
    * Suggestions
    
    * issue templates
    
    * issue projects
    
    * run add to project
    
    * bug
    
    * release version
    
    * readme logo
    
    * deploy all extensions script
    
    * correcting tag

---
+++ script/DownloadLatestReleases.ps1
@@ -0,0 +1,50 @@
+Param (
+    [bool] $BusinessCentralLocalization,
+    [bool] $TravelOrder,
+    [bool] $Manufacturing,
+    [bool] $EIN,
+    [bool] $HrmPayroll,
+    [bool] $EinLoc,
+    [bool] $Translation,
+    [string] $token
+)
+
+$repositoriesMap = @{"Business-Central-Localization" = $BusinessCentralLocalization
+    "Electronic-Invoicing"                           = $EIN
+    "Manufacturing-Management"                       = $Manufacturing
+    "Travel-Order"                                   = $TravelOrder
+    "HRM-and-Payroll"                                = $HrmPayroll
+    "CON_EIN_NPSLoc"                                 = $EinLoc
+    "Serbian-Translation-W1"                         = $Translation
+}
+$header=New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$header.Add("Authorization", "Bearer $token")
+$header.Add("Accept", "application/vnd.github+json")
+$header.Add("X-Github-Api-Version", "2022-11-28")
+function Get-Assets {
+    Param (
+        [string] $RepositoryName
+    )
+
+    $donwloadFolder = Join-Path $ENV:GITHUB_WORKSPACE ".artifacts"
+    $url = "https://api.github.com/repos/NPSBeograd/$RepositoryName/releases/latest"
+
+    $latestRelease = Invoke-RestMethod -Uri $url -Headers $header
+
+    # Select the first asset in the list
+    $firstAsset = $latestRelease.assets[0]
+
+    # Get the download URL of the first asset
+    $assetUrl = $firstAsset.browser_download_url
+
+    # Download the first asset
+    Invoke-WebRequest -Uri $assetUrl -OutFile  $donwloadFolder -Headers $header
+}
+
+foreach ($repositorieMap in $repositoriesMap.Keys){
+
+    if($repositoriesMap[$repositorieMap]){
+        Get-Assets -RepositoryName $repositorieMap
+    }
+}
+
---
+++ script/ResolveEnvironment.ps1
@@ -0,0 +1,23 @@
+Param(
+    [Parameter(HelpMessage = "Type of deployment (CD or Publish)", Mandatory = $false)]
+    [ValidateSet('CD','Publish')]
+    [string] $Publishing_Stategy = "CD",
+    [Parameter(HelpMessage = "fat or prod", Mandatory = $true)]
+    [ValidateSet('PROD*','FAT*')]
+    [string] $Environmet_deployment
+
+)
+
+
+$secret_value_string =""
+if ($Publishing_Stategy -eq "CD"){
+    $secret_value_string="QA"
+}else {
+    if($Environmet_deployment -eq "PROD*"){
+        $secret_value_string="PROD"}
+    elseif ($Environmet_deployment -eq "FAT*") {
+        $secret_value_string="FAT"
+    }
+}
+
+Write-Output $secret_value_string
