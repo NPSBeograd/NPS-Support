f39c8613e0a9da1283157ec1429d401a96ffaadf
Nemanja Zajic <zakizaja@hotmail.com>
2023-10-20 14:29:59 +0200

    a

--- script/DownloadLatestReleases.ps1
+++ script/DownloadLatestReleases.ps1
@@ -9,18 +9,18 @@ Param (
     [string] $token
 )
 
-$repositoriesMap = @{"Business-Central-Localization" = $BusinessCentralLocalization
-    "Electronic-Invoicing"                           = $EIN
-    "Manufacturing-Management"                       = $Manufacturing
-    "Travel-Order"                                   = $TravelOrder
-    "HRM-and-Payroll"                                = $HrmPayroll
-    "CON_EIN_NPSLoc"                                 = $EinLoc
-    "Serbian-Translation-W1"                         = $Translation
-}
-$header=New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$repositoriesMap = @(@{"Business-Central-Localization" = $BusinessCentralLocalization },
+    @{"Electronic-Invoicing" = $EIN },
+    @{"Manufacturing-Management" = $Manufacturing },
+    @{"Travel-Order" = $TravelOrder },
+    @{"HRM-and-Payroll" = $HrmPayroll },
+    @{"CON_EIN_NPSLoc" = $EinLoc },
+    @{"Serbian-Translation-W1" = $Translation }
+)
+$header = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $header.Add("Authorization", "Bearer $token")
 $header.Add("X-Github-Api-Version", "2022-11-28")
-$header.Add("Accept","")
+$header.Add("Accept", "")
 function Get-Assets {
     Param (
         [string] $RepositoryName
@@ -28,7 +28,7 @@ function Get-Assets {
 
     $donwloadFolder = Join-Path $ENV:GITHUB_WORKSPACE ".artifacts"
     $url = "https://api.github.com/repos/NPSBeograd/$RepositoryName/releases/latest"
-    $header["Accept"]= "application/vnd.github+json"
+    $header["Accept"] = "application/vnd.github+json"
 
     $latestRelease = Invoke-RestMethod -Uri $url -Headers $header
 
@@ -41,15 +41,15 @@ function Get-Assets {
     Write-Host "Browser url: $($firstAsset.browser_download_url)"
     Write-Host "Asset url: " $assetUrl
     # Download the first asset
-    $header["Accept"]= "application/octet-stream"
+    $header["Accept"] = "application/octet-stream"
     Invoke-WebRequest -Uri $assetUrl -OutFile  $donwloadFolder -Headers $header -Method Get
 }
 
-for ([int] $i=0; $i -le $repositoriesMap.Keys.Count; i++) {
+for ([int] $i = 0; $i -le $repositoriesMap.Count; i++) {
 
-    if([bool]$repositoriesMap[$repositoriesMap.Keys[$i]]){
-        Write-Host $repositoriesMap.Keys[$i]
-        Get-Assets -RepositoryName $repositoriesMap.Keys[$i]
+    if ([bool]$repositoriesMap[$i].values[0]) {
+        Write-Host $repositoriesMap[$i].Keys[0]
+        Get-Assets -RepositoryName $repositoriesMap[$i].Keys[0]
     }
 }
 
