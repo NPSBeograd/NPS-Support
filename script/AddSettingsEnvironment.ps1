09d0fb44dce2c1d6952b9f63297da81437020e71
Nemanja Zajic <zakizaja@hotmail.com>
2023-10-23 12:39:47 +0200

    a

--- script/AddSettingsEnvironment.ps1
+++ script/AddSettingsEnvironment.ps1
@@ -3,15 +3,22 @@ Param(
     [string] $ResolvedEnvironmentName
 )
 
-
 # Path to the JSON file
 $jsonFilePath = ".github/AL-Go-Settings.json"
 
 # Read the JSON content from the file
 $jsonContent = Get-Content -Path $jsonFilePath | ConvertFrom-Json
 
-# Modify the JSON content (for example, add a new property)
-$jsonContent.environments = "$EnvironmentName ($ResolvedEnvironmentName)"
+
+# Check if 'environments' property exists, if not, create it as an empty array
+if (-not $jsonContent.PSObject.Properties["environments"]) {
+    $jsonContent | Add-Member -MemberType NoteProperty -Name environments -Value @()
+}
+
+# Add a new string to the 'environments' array
+$jsonContent.environments += "$EnvironmentName ($ResolvedEnvironmentName)"
 
 # Write the modified JSON content back to the file
 $jsonContent | ConvertTo-Json | Set-Content -Path $jsonFilePath
+ 
+
