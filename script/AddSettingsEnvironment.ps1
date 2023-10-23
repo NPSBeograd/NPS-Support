b7c2ec9eb8e0d93f8406888b5a3c2699c022640c
Nemanja Zajic <zakizaja@hotmail.com>
2023-10-23 21:34:46 +0200

    a

--- script/AddSettingsEnvironment.ps1
+++ script/AddSettingsEnvironment.ps1
@@ -1,8 +1,21 @@
 Param(
     [string] $EnvironmentName,
-    [string] $ResolvedEnvironmentName
+
+    [ValidateSet('','PROD*','FAT*')]
+    [string] $Environmet_deployment=""
 )
 
+
+$secret_value_string =""
+if ($Environmet_deployment -eq ""){
+    $secret_value_string="QA"
+}else {
+    if($Environmet_deployment -eq "PROD*"){
+        $secret_value_string="PROD"}
+    elseif ($Environmet_deployment -eq "FAT*") {
+        $secret_value_string="FAT"
+    }
+}
 
 # Path to the JSON file
 $jsonFilePath = ".github/AL-Go-Settings.json"
@@ -17,7 +30,7 @@ if (-not $jsonContent.PSObject.Properties["environments"]) {
 }
 
 # Add a new string to the 'environments' array
-$jsonContent.environments += "$EnvironmentName ($ResolvedEnvironmentName)"
+$jsonContent.environments += "$EnvironmentName ($secret_value_string)"
 
 # Write the modified JSON content back to the file
 $jsonContent | ConvertTo-Json | Set-Content -Path $jsonFilePath
