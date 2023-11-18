b2ca8ca3aab72e0553a036d080aef9991339e26e
Nemanja Zajic <34772552+nemanja899@users.noreply.github.com>
2023-11-18 19:40:03 +0100

    Deploy (#53)
    * artifact name
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a

--- script/DownloadLatestReleases.ps1
+++ script/DownloadLatestReleases.ps1
@@ -10,8 +10,9 @@ $header.Add("X-Github-Api-Version", "2022-11-28")
 $header.Add("Accept", "")
 
 
-Write-Host $Repository
-$donwloadFolder = Join-Path $ENV:GITHUB_WORKSPACE ".$Repository"
+
+$donwloadFolder =  $ENV:GITHUB_WORKSPACE
+
 $url = "https://api.github.com/repos/NPSBeograd/$Repository/releases/latest"
 $header["Accept"] = "application/vnd.github+json"
 
@@ -23,15 +24,17 @@ $firstAsset = $latestRelease.assets[0]
 
 # Get the download URL of the first asset
 $assetUrl = "https://api.github.com/repos/NPSBeograd/$Repository/releases/assets/$($firstAsset.id)"
-
+$filePath= $("$donwloadFolder/$Repository/$Repository.zip")
 Write-Host " Downloading Repository $Repository asset"
 Write-Host "Asset url: " $assetUrl
+Write-Host "File path: $filePath"
 # Download the first asset
 $header["Accept"] = "application/octet-stream"
-Invoke-WebRequest -Uri $assetUrl -OutFile  $donwloadFolder -Headers $header -Method Get
+Invoke-WebRequest -Uri $assetUrl -OutFile $filePath  -Headers $header -Method Get
 
-$downloadName = ls $donwloadFolder
+ls $donwloadFolder
+$downloadName = "$Repository.zip"
+
 Add-Content -Encoding UTF8 -Path $env:GITHUB_OUTPUT -Value "DownloadedArtifactName=$downloadName"
 
 Write-Host "Downloading Completed in folder $donwloadFolder..."
-Start-Sleep -Seconds 10
